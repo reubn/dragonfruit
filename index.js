06c94aa6b6d16808b5701ff2204c3d271d610238
Reuben <reuben.eggar@me.com>
2015-11-09 15:47:27 +0000

    Refinements

--- index.js
+++ index.js
@@ -15,27 +15,28 @@ module.exports = function(content) {
         return classNameMappings[Object.keys(classNameMappings)[0]];
       }
 
-      function extractClassNames(args, noWrap) {
+      function extractClassNames(args) {
+
         //No args supplied
         if(!args.length) return false;
 
         //classNames are in first argument
         if (args.length === 1) {
           //classNames are an array
-          if (Array.isArray(args[0])) return extractClassNames(args[0], true);
+          if (Array.isArray(args[0])) return extractClassNames(args[0]);
 
           //classNames are an object
-          if (Object.prototype.toString.call(args[0]) === "[object Object]") return extractClassNames(Object.keys(args[0]).filter(function(key){return !!args[0][key]}), true);
+          if (Object.prototype.toString.call(args[0]) === "[object Object]") return extractClassNames(Object.keys(args[0]).filter(function(key){return !!args[0][key]}));
 
           //One className as string
-          if (typeof args[0] === "string") return noWrap?args[0]:[args[0]];
+          if (typeof args[0] === "string") return [args[0]];
 
           //classNames returned by function
-          if (Object.prototype.toString.call(args[0]) === "[object Function]") return extractClassNames([args[0](classNameMappings)], true);
+          if (Object.prototype.toString.call(args[0]) === "[object Function]") return extractClassNames([args[0](classNameMappings)]);
         } else {
           //classNames are arguments
-          return [].concat.apply([],args.map(function(arg) {
-            return extractClassNames([arg], true);
+          return [].concat.apply([], args.map(function(arg) {
+            return extractClassNames([arg]);
           }));
         }
       }
