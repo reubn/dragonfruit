4de3929be5b118099489937ef841ef8de9b582c8
Reuben <reuben.eggar@me.com>
2015-11-09 14:29:02 +0000

    Bugfixes

--- index.js
+++ index.js
@@ -1,5 +1,44 @@
 module.exports = function(content) {
-    var done = content + '"development"===process.env.NODE_ENV&&console.log("Content:",content),module.exports=function(){function n(e){return e?!1:1!==e.length?e.map(function(t){return n([t])}):Array.isArray(e[0])?e[0]:"string"==typeof e[0]?[e[0]]:"[object Function]"==Object.prototype.toString.call(e[0])?n([e[0](t)]):void 0}var t=content.locals;return function(){var e=n(arguments);return"development"===process.env.NODE_ENV&&console.log("arrayOfClassNames:",e),e!==!1?e.map(function(n){return t[n]}).join(" "):t[Object.keys(t)[0]]}}(content)';
+    var done = content + `
+    if (process.env.NODE_ENV === "development") console.log("Content:", content);
+    module.exports = function() {
+      var classNameMappings = content.locals;
+      return function() {
+        var arrayOfClassNames = extractClassNames(arguments);
+        if (process.env.NODE_ENV === "development") console.log("arguments:", arguments);
+        if (process.env.NODE_ENV === "development") console.log("arrayOfClassNames:", arrayOfClassNames);
+
+        //If we have classNames, return className string
+        if(arrayOfClassNames !== false) return arrayOfClassNames.map(function(className){return classNameMappings[className]}).join(" ");
+
+        //If we don't have classNames, return first className
+        return classNameMappings[Object.keys(classNameMappings)[0]];
+      }
+
+      function extractClassNames(args, noWrap) {
+        //No args supplied
+        if(!args.length) return false;
+
+        //classNames are in first argument
+        if (args.length === 1) {
+          //classNames are an array
+          if (Array.isArray(args[0])) return extractClassNames(args[0], true);
+
+          //One className as string
+          if (typeof args[0] === "string") return noWrap?args[0]:[args[0]];
+
+          //classNames returned by function
+          if (Object.prototype.toString.call(args[0]) === "[object Function]") return extractClassNames([args[0](classNameMappings)]);
+        } else {
+          //classNames are arguments
+          return args.map(function(arg) {
+            return extractClassNames([arg], true);
+          });
+        }
+      }
+
+    }(content);
+`;
     var callback = this.async();
     if(!callback) return done;
     callback(null, done)
